1f2bc5be94b7b43669abc67b9c830d586f2f04c4
Alfredo <manjon.canela@gmail.com>
2021-02-04 20:38:57 +0100

    add download buttons and fonts

--- app/public/wp-content/themes/pickappTheme/functions.php
+++ app/public/wp-content/themes/pickappTheme/functions.php
@@ -10,7 +10,9 @@ add_action('init', 'pickApp_menus');
 
 function pickApp_scripts_styles()
 {
-    wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0.0');
+    wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css2?family=PT+Serif:wght@400;700&display=swap', array(), '1.0.0');
+
+    wp_enqueue_style('style', get_stylesheet_uri(), array('googleFont'), '1.0.0');
 }
 
 add_action('wp_enqueue_scripts', 'pickApp_scripts_styles');
--- app/public/wp-content/themes/pickappTheme/index.php
+++ app/public/wp-content/themes/pickappTheme/index.php
@@ -23,9 +23,25 @@
 
             </div>
         </div>
+        <div class="dev-mesage">
+            <div class="info-icon">
+                <img class="info-img" src="<?php echo get_template_directory_uri(); ?>/img/info_icon2.png" alt="Apple Download Button">
+            </div>
+            <div class="info-text">
+                <p>Actualmente este proyecto se encuentra en fase de desarrollo, no abierto al público general</p>
+            </div>
+        </div>
         <p class="title">PickApp</p>
         <p class="desc">Los productos de siempre, ahora online</p>
         <img class="img" src="<?php echo get_template_directory_uri(); ?>/img/phone.png" alt="logo pagina">
+        <div class="row download-container">
+            <div class="column ios-download">
+                <img class="download-img" src="<?php echo get_template_directory_uri(); ?>/img/apple_download.png" alt="Apple Download Button">
+            </div>
+            <div class="column android-container">
+                <img class="download-img" src="<?php echo get_template_directory_uri(); ?>/img/android_download.png" alt="Android Download Button">
+            </div>
+        </div>
     </div>
 </body>
 
